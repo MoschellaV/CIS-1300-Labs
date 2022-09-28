96a8ad270c0d31d0ddc00fa17d8c0ac86126e07e
MoschellaV <58868225+MoschellaV@users.noreply.github.com>
2022-09-28 11:25:29 -0400

    modified Lab3

--- Lab3/L3P1.c
+++ Lab3/L3P1.c
@@ -1,22 +1,5 @@
 #include <stdio.h>
 
-float add(float num1, float num2){
-    /* adds the two numbers */
-    return num1+num2;
-}
-float subtract(float num1,float num2){
-    /* subtracts the two numbers */
-    return num1-num2;
-}
-float multiply(float num1,float num2){
-    /* multiplys the two numbers */
-    return num1*num2;
-}
-float divide(float num1, float num2){
-    /* divides the two numbers */
-    return num1/num2;
-}
-
 int main(){
     
     // declare variables
@@ -40,22 +23,22 @@ int main(){
 
         // if operator is equal to +
         if (operator == 43){
-            printf("%.2f + %.2f = %.2f \n", num1, num2, add(num1,num2));
+            printf("%.2f + %.2f = %.2f \n", num1, num2, num1+num2);
         }
         // if operator is equal to -
         else if (operator == 45){
-            printf("%.2f - %.2f = %.2f \n", num1, num2, subtract(num1,num2));
+            printf("%.2f - %.2f = %.2f \n", num1, num2, num1-num2);
         }
         // if operator is equal to *
         else if (operator == 42){
-            printf("%.2f * %.2f = %.2f \n", num1, num2, multiply(num1,num2));
+            printf("%.2f * %.2f = %.2f \n", num1, num2, num1*num2);
         }
         // if operator is equal to /
         else if (operator == 47){
 
             // check if denominator is valid
             if(num2 != 0){
-                printf("%.2f / %.2f = %.2f \n", num1, num2, divide(num1,num2));
+                printf("%.2f / %.2f = %.2f \n", num1, num2, num1/num2);
             } else {
                 printf("Denominator cannot be 0 \n");
         }
