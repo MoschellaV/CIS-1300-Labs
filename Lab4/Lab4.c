a17c9dbb2576b43e9e34d4e6f03a630a56c6bf0c
MoschellaV <58868225+MoschellaV@users.noreply.github.com>
2022-10-05 11:43:11 -0400

    small change

--- Lab4/Lab4.c
+++ Lab4/Lab4.c
@@ -25,14 +25,12 @@ int main(){
     bool run = true;
     char userContinue;
 
-    printf("What loop do you want to use - enter 1, 2, or 3: ");
+    printf("\nWhat loop do you want to use - enter 1, 2, or 3: ");
     scanf("%d", &userChoice);
 
     /* LOOP 1 */
     if (userChoice == 1){
-
         printf("\nThis loop runs 3 times\n\n");
-        
         // loop iteates 3 times
         for(int i = 0; i < 3; i++){
             printf("Enter radius: ");
@@ -43,23 +41,21 @@ int main(){
     
     /* LOOP 2 */
     if (userChoice == 2){
+        printf("\nThis loop runs until the radius entered is -1\n\n");
         // while loop runs until user enters -1 as the radius
         while (run) 
         {
             printf("Enter radius: ");
             scanf("%f", &radius);
-            circumferenceAndArea(radius);
-
             if (radius == -1) run = false; 
+            else circumferenceAndArea(radius);
         }
         
     }
 
     /* LOOP 3 */
     if (userChoice == 3){
-
         printf("\nThis Loop runs until the user does not wish to continue\n\n");
-
         // do while loop runs until user does not enter 'y' to continue
         do{   
             printf("Enter radius: ");
