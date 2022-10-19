dec9c96f828227f1c2cba61c45aa391a7d878f69
MoschellaV <58868225+MoschellaV@users.noreply.github.com>
2022-10-19 11:47:20 -0400

    alter comments

--- Lab6/lab6.c
+++ Lab6/lab6.c
@@ -25,19 +25,15 @@ int main(){
       
       result = compute (aNumber, anotherNumber, operator);
 
-      // If denominator is 0
       if (result == -1) {
          printf ("When dividing, denominator cannot be 0 \n");
       }
 
-      // If denominator is not 0
       else {
-         
          printf ("%.2lf %c %.2lf = %.2lf\n", aNumber, operator, anotherNumber, result);
       }
    }
    
-   // If user inputs a invalid operator 
    else {
       printf("%c is an invalid operator \n", operator);
    }
@@ -47,10 +43,6 @@ int main(){
 
 char readOperator (void) {
    /* This function reads a character and returns it
- 
-   Function name: readOperator
-   Inputs: None
-   Returns: char
    */
 
    char operator;
@@ -61,33 +53,27 @@ char readOperator (void) {
 }
 
 int isOperatorValid (char operator) {
-   /* This function takes a character as input and returns 1 if the character is a valid operator; 0 otherwise
+   /* This function takes a character as input and 
+   returns 1 if the character is a valid operator; 0 otherwise
 
-   Function name: isOperatorValid
-   Inputs: char
-   Returns: int
+   43 is the ascii of +
+   45 is the ascii of -
+   42 is the ascii of *
+   47 is the ascii of /
+
    */
 
-    // 43 is the ascii of +
-    // 45 is the ascii of -
-    // 42 is the ascii of *
-    // 47 is the ascii of /
-
-    // If operator is valid
+    // validates operator
     if ((operator == 43) || (operator == 45) || (operator == 42) || (operator == 47)) return 1;
 
-    // If operator is invalid
     else return 0;
         
 }
 
 
 double readANumber (void) {
-   /* This function reads a number as double and returns it
- 
-   Function name: readANumber
-   Inputs: None
-   Returns: double
+   /* This function reads a number as double 
+   and returns it
    */
 
    double number;
@@ -100,23 +86,19 @@ double readANumber (void) {
 
 
 double compute (double aNumber, double anotherNumber, char operator) {
-   /* This function applies the given operator to 2 given numbers and returns the computed value.
+   /* This function applies the given operator to two given numbers 
+   and returns the computed value.
 
-   Function name: compute
-   Inputs: double, double, char
-   Returns: double
    */
 
-   // if user wants to add
+   // computes calculation based on the selected operator 
+
    if (operator == 43) return aNumber + anotherNumber;
 
-   // If user wants to subtract
    else if (operator == 45) return aNumber - anotherNumber;
 
-   // If user wants to multiply
    else if (operator == 42) return aNumber*anotherNumber;
 
-   // If user wants to divide
    else{
       if (anotherNumber == 0) return -1;
       else return aNumber/anotherNumber;
