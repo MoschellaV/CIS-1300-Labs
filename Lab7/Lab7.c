9e6b13a453bce8d93b16e9ea3aa980ba1c674d78
Adrian Di Tosto <amditosto@gmail.com>
2022-10-25 22:18:32 -0400

    Lab 7

---
+++ Lab7/Lab7.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int MAX = 5;
+
+void readArray (int anArray [MAX]);
+void reverseArray (int original [MAX], int reversed [MAX]);
+void printArray (int anArray [MAX]);
+
+int main(){
+
+   int anArray [MAX], newArray [MAX];
+   
+   printf ("Testing function anArray \n\n");
+
+   readArray(anArray);
+
+   printf ("Testing function reverseArray \n\n");
+   reverseArray(anArray, newArray);
+
+   return 0;
+}
+
+void readArray(int* anArray){
+    /*Function takes in array as input*/
+   printf("Enter 5 elements\n");
+   scanf("%d%d%d%d%d",&anArray[0],&anArray[1],&anArray[2],&anArray[3],&anArray[4]);
+   printf("\n\n");
+   }
+
+void reverseArray (int* original, int* reversed){
+    /*Function takes reverses array*/
+   int count = 4;
+
+   printf("Original Array\n");
+   printArray(original);
+
+   printf("Reversed Array");
+
+   for (int i = 0; i < 5; i++){
+      reversed[i] = original[count];
+      count --;
+   }
+   printf("\n");
+   printArray(reversed);
+
+}
+
+void printArray (int* anArray) {
+    /*Function prints array*/
+
+   printf("%d %d %d %d %d\n",anArray[0],anArray[1],anArray[2],anArray[3],anArray[4]);
+
+}
+
+
