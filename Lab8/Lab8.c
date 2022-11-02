655c2085d6f8ae5bbaefc32c40181aa7e2d3d9d7
MoschellaV <58868225+MoschellaV@users.noreply.github.com>
2022-11-02 02:02:30 -0400

    Lab8

--- Lab8/Lab8.c
+++ Lab8/Lab8.c
@@ -1,8 +1,75 @@
-# include <stdio.h>
+#include <stdio.h>
+# include <string.h>
 
-int main(){
+#define N 4
 
+void print2D (int arr[N][N]) ;
+int isLeftdEqualToRightd (int arr [N][N], int *, int *);
 
-    return 0; 
+/////////////////////////////////
+int main () {
+
+   int arr [N][N];
+   int i, j;
+   int result, sumLeftD, sumRightD;
+   
+   for (i = 0; i < N; i++) {
+      for (j = 0; j < N; j++) {
+          scanf ("%d", &arr [i][j]);
+      }
+    }
     
+    result = isLeftdEqualToRightd (arr, &sumLeftD, &sumRightD);
+    
+    print2D(arr);
+    
+    printf ("Sum of left diagonal = %d\n", sumLeftD);
+    printf ("Sum of right diagonal = %d\n", sumRightD);
+    
+    if (result) {
+       printf ("Sum of left diagonal is equal to sum of right diagonal\n");
+    }
+    else {
+        printf ("Sum of left diagonal is not equal to sum of right diagonal\n");
+    }
+    
+   return 0;
+}
+/////////////////////////////////
+
+void print2D(int arr[N][N]){
+
+    int i,j;
+    for ( i = 0; i < N; i++) { 
+        printf("Row %d: ",i+1);
+        for ( j = 0; j < N; j++) { 
+            printf("%d ",arr[i][j]);
+        } 
+        printf("\n");
+    } 
+}
+
+int isLeftdEqualToRightd(int arr[N][N],int* left,int* right){
+
+    int count = 0;
+    int count2 = N-1;
+    int sum1 = 0;
+    int sum2 = 0;
+
+    // left to right diagonal
+    for(int k = 0; k < N; k++){
+        sum1 += arr[k][count];
+        count++;
+    }
+    *left = sum1;
+    
+    // right to left diagonal
+    for(int k = 0; k < N; k++){
+        sum2 += arr[k][count2];
+        count2--;
+    }
+    *right = sum2;
+    
+    if (*left == *right) return 1;
+    else return 0;
 }
